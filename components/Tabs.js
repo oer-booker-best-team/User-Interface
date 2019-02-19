530639eea463c87094f73a3879033ac1426206f1
Poly Goveia <poly.goveia@gmail.com>
2019-02-19 11:25:16 -0800

    remove and add class for tabs

--- components/Tabs.js
+++ components/Tabs.js
@@ -3,11 +3,9 @@ class TabLink {
     constructor(element) {
         // Assign this.element to the passed in DOM element
         this.element = element;
-        console.log(this.element);
 
         // Get the custom data attribute on the Link
         this.data = this.element.dataset.tab;
-        console.log(this.data);
 
         // Using the custom data attribute get the associated Item element
         this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
@@ -21,10 +19,11 @@ class TabLink {
 
     select() {
         // Get all of the elements with the tabs-link class
-        const links = document.querySelectorAll('.tabs-link');;
+        const links = document.querySelectorAll('.tabs-link');
 
         // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-        Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
+        // Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
+          links.forEach(link => link.classList.remove("tabs-link-selected"));
 
         // Add a class named "tabs-link-selected" to this link
         this.element.classList.add('tabs-link-selected');
@@ -51,7 +50,7 @@ class TabItem {
         // Add a class named "tabs-item-selected" to this element
         this.element.classList.add('tabs-item-selected');
 
-        this.element.style.display = "flex";
+       
     }
 }
 
